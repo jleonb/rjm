838d70e11dc8f8bf17840e51d4cc71bbaeec2dec
jleon <jleon@webframe.cl>
2016-07-18 13:46:17 -0400

    últimos cambios
    servicios - emerson - contacto

--- contacto.php
+++ contacto.php
@@ -1,37 +1,37 @@
 <?php
 if(isset($_POST['email'])) {
- 
+
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "ruben@rjmasesores.c";
+    $email_to = "ruben@rjmasesores.cl";
     $email_subject = "CONTACTO - WEB";
- 
+
     function died($error) {
         // your error code can go here
         echo "Lo sentimos, hemos encontrado un error. ";
         echo $error."<br /><br />";
         die();
     }
- 
-   
- 
+
+
+
     $nombre = $_POST['nombre']; // required
     $telefono = $_POST['telefono']; // required
-    $email = $_POST['email']; // required    
+    $email = $_POST['email']; // required
     $comments = $_POST['comments']; // required
- 
+
 
     $email_message = "Los datos del interesado son:\n\n";
- 
+
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
- 
+
     $email_message .= "Nombre: ".clean_string($nombre)."\n";
     $email_message .= "Teléfono: ".clean_string($telefono)."\n";
-    $email_message .= "Correo: ".clean_string($email)."\n\n";  
+    $email_message .= "Correo: ".clean_string($email)."\n\n";
     $email_message .= "Comentario: ".clean_string($comments)."\n";
- 
+
 // create email headers
 $headers = 'From: '.$email."\r\n".
 'Reply-To: '.$email."\r\n" .
@@ -40,18 +40,7 @@ $headers = 'From: '.$email."\r\n".
 sleep(2);
 echo "<meta http-equiv='refresh' content=\"0; url=./index.html\">";
 ?>
- 
+
 <?php
 }
 ?>
-
-
-
-
-
-
-
-
-
-
-
